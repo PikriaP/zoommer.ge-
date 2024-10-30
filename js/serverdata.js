5d0454be0352a306df8bea7906eb04085b8c0a05
piqriap <102671865+PikriaP@users.noreply.github.com>
2024-10-30 17:49:35 +0400

    add  local json

--- js/serverdata.js
+++ js/serverdata.js
@@ -1,49 +1,29 @@
-function fetchJSONData() {
-    fetch("./products.json")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then((data) => displayProducts(data))
-      .catch((error) => console.error("Unable to fetch data:", error));
-  }
-  fetchJSONData();
-  
-  const displayProducts = (data) => {
-    div.innerHTML = "";
-  }
-  
 
-  
-    data.forEach((product) => {
-      const div = document.getElementById("div");
-      const container = document.c
-  function fetchJSONData() {
-    fetch("./products.json")
+function fetchJSONData() {
+  fetch("./products.json")
       .then((res) => {
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-        return res.json();
+          if (!res.ok) {
+              throw new Error(`HTTP error! Status: ${res.status}`);
+          }
+          return res.json();
       })
       .then((data) => displayProducts(data))
       .catch((error) => console.error("Unable to fetch data:", error));
-  }
-  fetchJSONData();
-  const displayProducts = (data) => {
-    div.innerHTML = "";
-  
-    data.forEach((product) => {
-      const div = document.getElementById("div");
+}
+
+const displayProducts = (data) => {
+  const div = document.getElementById("div");
+  div.innerHTML = ""; // Clear existing content
+
+  data.forEach((product) => {
       const container = document.createElement("div");
       container.innerHTML = `
-      <img src=${product.imageUrl}/>
-      <p>${product.name}</p>
-      <p>${product.price}</p>
+          <img src="${product.imageUrl}" alt="${product.name}" />
+          <p>${product.name}</p>
+          <p>${product.price}</p>
       `;
       div.appendChild(container);
-    });
-  };
-    
+  });
+};
+
+fetchJSONData();
